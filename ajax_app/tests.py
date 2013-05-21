548c83b69b228fc4a0ba487e401a6f07c5d124e8
Tim Garner <timgarner0@gmail.com>
2013-05-21 13:01:17 +0100

    Update tests

--- ajax_app/tests.py
+++ ajax_app/tests.py
@@ -1,6 +1,7 @@
 from django.db import IntegrityError
-from django.test import TestCase
+from django.test import TestCase, Client
 import pytz
+from ajax_app.forms import BookForm, ConfirmForm
 from ajax_app.models import *
 from datetime import datetime
 from cisco_middleware import config, test_snmp
@@ -39,8 +40,51 @@ class BookTest(TestCase):
                           config_set=self.config_set)
 
 
+class FormTest(TestCase):
+
+    fixtures = ['ajax_app/fixtures/initial_data.json']
+
+    def test_booking_valid(self):
+        data = {'study_type': 1,
+                'start_date': '21/05/2013',
+                'start_time': '12:00:00',
+                'end_date': '21/05/2013',
+                'end_time': '13:00:00'}
+        form = BookForm(data=data)
+        self.assertEqual(form.is_valid(), True)
+
+    def test_booking_invalid(self):
+        data = {'study_type': 1,
+                'start_date': '21/05/2013',
+                'start_time': '12:00:00',
+                'end_date': '21/05/2013',
+                'end_time': '12:00:00'}
+        form = BookForm(data=data)
+        self.assertEqual(form.is_valid(), False)
+
+    def test_confirm_valid(self):
+        data = {'config_set': 2}
+        form = ConfirmForm(data=data, pod_id=1, username='tim')
+        self.assertEqual(form.is_valid(), True)
+
+    def test_confirm_invalid(self):
+        data = {'config_set': 1}
+        form = ConfirmForm(data=data, pod_id=1, username='tim')
+        self.assertEqual(form.is_valid(), False)
+
 
 class SNMPTest(TestCase):
+
+    fixtures = ['ajax_app/fixtures/initial_data.json']
+
+    def test_high_level_get(self):
+        result = config.get_config('192.168.1.1')
+        self.assertEqual(result, 'Got config for 192.168.1.1')
+
+    def test_high_level_set(self):
+        conf_set = ConfigSet.objects.get(pk=3)
+        result = config.configure_device_group(conf_set)
+        self.assertEqual(result, None)
 
     def test_get(self):
         """Test a low level GET response"""
@@ -55,17 +99,28 @@ class SNMPTest(TestCase):
 
 class APITest(TestCase):
 
-    def test_creation(self):
-        """Make a booking through API. Check it exists"""
-        # api = slumber.API("http://127.0.0.1:8000/api/v1/")
-        # booking = api.booking.post({
-        #     "start_datetime": "2013-04-20T10:00:00",
-        #     "end_datetime": "2013-04-20T11:00:00",
-        #     "pod": "/api/v1/pod/1/",
-        #     "config_set": "/api/v1/configset/13/",
-        #     "study_type": "CCNA",
-        #     "user": "tim"
-        # })
+    fixtures = ['ajax_app/fixtures/initial_data.json']
+
+    def test_get_booking(self):
+        client = Client()
+        response = client.get('/api/v1/booking/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_pod(self):
+        client = Client()
+        response = client.get('/api/v1/pod/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_device(self):
+        client = Client()
+        response = client.get('/api/v1/device/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_availability(self):
+        client = Client()
+        response = client.get('/api/v1/availability/?end=2013-03-05%2013%3A00%3A00&start=2013-03-05%2012%3A00%3A00&study_type=CCNA')
+        self.assertEqual(response.status_code, 200)
+
 
 
 
